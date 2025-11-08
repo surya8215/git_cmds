e4b940f1ef60448e90e0e8539c934832132a6c9f
Surya Akkisetti <suryaakkisetti2@gmail.com>
2025-11-08 16:20:59 +0530

    forthcommit

---
+++ new.py
@@ -0,0 +1,6 @@
+name = "Alice"  # String
+age = 30        # Integer
+height = 5.7    # Float
+is_student = True # Boolean
+
+print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")
